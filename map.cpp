6ef02c215bfd3829e991ddd33fabd9a677a2833d
“Airzh1124” <u3609849@connect.hku.hk>
2024-04-12 15:43:09 +0800

    add x-y axis to the print function

--- map.cpp
+++ map.cpp
@@ -31,16 +31,23 @@ public:
     }
 
     void printPMap() {
+        cout << setw(2) << " " << " ";
+        for (int j = 0; j < grid[0].size(); ++j) {
+            cout << setw(2) << j << " ";
+        }
+        cout << endl;
+        int rownumber = 0;
         for (int i = 0; i < grid.size(); i++) {
+            cout << setw(2) <<rownumber++ << " ";
             for (int j = 0; j < grid[i].size(); j++) {
                 if (grid[i][j] == 0){
-                    cout << "O ";
+                    cout << setw(2) << "." << " ";
                 }
                 if (grid[i][j] == 1){
-                    cout << "- ";
+                    cout << setw(2) << "-" << " ";
                 }
                 if (grid[i][j] == 2){
-                    cout << "X ";
+                    cout << setw(2) << "X" << " ";
                 }
             }   
             cout << endl;
@@ -48,15 +55,42 @@ public:
     }
 
     void printCMap() {
+        cout << setw(2) << " " << " " ;
+        for (int j = 0; j < grid[0].size(); ++j) {
+            cout << setw(2) << j << " ";
+        }
+        cout << endl;
+        int rownumber = 0;
         for (int i = 0; i < grid.size(); i++) {
+            cout << setw(2) << rownumber++ << " ";
             for (int j = 0; j < grid[i].size(); j++) {
                 if (grid[i][j] == 0){
-                    cout << "O ";
+                    cout << setw(2) << "." << " ";
                 }else{
-                    cout << "X ";
+                    cout << setw(2) << "X" << " ";
                 }
             }   
             cout << endl;
         }
     }
 };
+
+int main() {
+    Map map(20,20);
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            if (i == j)
+            {
+                map.setCell(i, j, 1);
+            }
+            if (i > j)
+            {
+                map.setCell(i, j, 2);
+            }
+            
+        }
+    }
+    map.printPMap();
+    map.printCMap();
+    return 0;
+}
