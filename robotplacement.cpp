3eead87e0cddec590f160404b322934b226b9db5
banana <yinjie@connect.hku.hk>
2024-04-14 18:31:57 +0800

    “”

---
+++ robotplacement.cpp
@@ -0,0 +1,23 @@
+#include "map.h"
+#include "shipplacement.h"
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+//g++ -pedantic-errors -std=c++11 robotplacement.cpp map.cpp  -o test
+
+int main(){
+    int x, y, value, placementcout = 0, Shiplen, Shipwei, Shiprow, Shipcol;
+    cout << "Please enter the size of the map (row column)." << endl;
+    cin >> x >> y;
+    Map m1(x, y);
+    while (Shipshape.empty() == false){
+        m1.printPMap();
+        getAndplaceShape(m1);
+    }
+    cout << "Here is your final version of map: " << endl;
+    m1.printPMap();
+
+return 0;
+}
