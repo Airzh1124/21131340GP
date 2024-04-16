7cd1b2d0844362cfa0fe932c81df97527ea7abab
IvanNing <160609422+IvanNing@users.noreply.github.com>
2024-04-16 13:26:53 +0800

    Update playeractions.h

--- ncurses/playeractions.h
+++ ncurses/playeractions.h
@@ -1,1 +1,18 @@
-include
+#pragma once
+
+#include <ncurses.h>
+#include <vector>
+#include map.h
+
+int choosePosition(int row, int col);
+// choose a position and check which integer it is, if it is a nonzero number, the delete it from the map.
+
+bool gameStatus(Map &map);
+// check if the map still contain any nonzero number, if over, return true.
+
+void shipSink(Map &map, int shipNumber);
+// change the ship to the sink status
+
+
+
+
