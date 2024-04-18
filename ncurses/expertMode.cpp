d24ef101b402ad6fe45280ffd10636472b2def2d
IvanNing <160609422+IvanNing@users.noreply.github.com>
2024-04-19 01:32:06 +0800

    Update expertMode.cpp

--- ncurses/expertMode.cpp
+++ ncurses/expertMode.cpp
@@ -31,8 +31,6 @@ void instruction(const string &s) {
 }
 
 void masterRobotattack(Map &map, Map &map1, int &ran){
-    int Shipcol, Shiprow;
-    int count = 1;
     vector<vector<int>> targets;
     for (int i = 0; i < map.getCols(); i++){
             for (int j = 0; j < map.getRows(); j++){
@@ -42,35 +40,6 @@ void masterRobotattack(Map &map, Map &map1, int &ran){
               } 
             }
     }
-    if (count % 3 != 0){  //每三次作弊一次
-      do{
-          ran++;
-          Shipcol = randomgenerator(map.getCols()+1, 0, ran);
-          Shiprow = randomgenerator(map.getRows()+1, 0, Shipcol);
-      }while (map1.getCell(Shiprow, Shipcol) != 0);
-      int value = map.getCell(Shiprow, Shipcol);
-
-      if (value == 0){
-          map1.setCell(Shiprow, Shipcol, 1);
-          map1.printDMap();
-      }
-      else{
-          for (int i = 0; i < map.getCols(); i++){
-              for (int j = 0; j < map.getRows(); j++){
-                  if (map.getCell(i, j) == value){
-                      map1.setCell(i, j, value);
-                      map.setCell(i, j, 1);
-                      map1.printDMap();
-                  }
-              }
-          }
-      }
-      instruction("The robot had attacked the coordinate: ");
-      printw("%d %d", Shiprow, Shipcol);
-      return;
-    }
-  else{
-    ran++;
     int chosen = randomgenerator(0, targets.size() - 1, ran); //作弊选中一个vector
     int i = targets[chosen][0]
     int j = targets[chosen][1]
