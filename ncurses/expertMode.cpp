7e6c6f063d3b697efcd09c06154776b6be1982ee
IvanNing <160609422+IvanNing@users.noreply.github.com>
2024-04-25 18:57:35 +0800

    Update expertMode.cpp

--- ncurses/expertMode.cpp
+++ ncurses/expertMode.cpp
@@ -1,10 +1,10 @@
 #include "expertMode.h"
 
-void masterRobotattack(Map &map, Map &map1, int &ran){
+void masterRobotattack(Map &map1, Map &map4, int &ran){
     vector<vector<int>> targets;
-    for (int i = 0; i < map.getCols(); i++){
-            for (int j = 0; j < map.getRows(); j++){
-              if (map.getCell(i,j) != 0 && map.getCell(i,j) != 1){ //找出所有存有船只的位置，并以vector形式存储
+    for (int i = 0; i < map1.getCols(); i++){
+            for (int j = 0; j < map1.getRows(); j++){
+              if (map1.getCell(i,j) != 0 && map1.getCell(i,j) != 1){ //找出所有存有船只的位置，并以vector形式存储
                 vector<int> a = {i, j};
                 targets.push_back(a);
               } 
@@ -13,13 +13,13 @@ void masterRobotattack(Map &map, Map &map1, int &ran){
     int chosen = randomgenerator(targets.size(), 0, ran); //作弊选中一个vector
     int x = targets[chosen][0];
     int y = targets[chosen][1];
-    int value = map.getCell(x, y);
-    for (int i = 0; i < map.getCols(); i++){
-            for (int j = 0; j < map.getRows(); j++){
+    int value = map1.getCell(x, y);
+    for (int i = 0; i < map1.getCols(); i++){
+            for (int j = 0; j < map1.getRows(); j++){
                 if (map.getCell(i, j) == value){
-                    map1.setCell(i, j, value);
-                    map.setCell(i, j, 1);
-                    map1.printDMap();
+                    map4.setCell(i, j, value);
+                    map1.setCell(i, j, 1);
+                    map4.printDMap();
                 }
             }
         }
