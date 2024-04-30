fff1afbe08868ea0d2c5ddcc84b7d750e9cb7916
HarrisonFong <157101919+HarrisonFong@users.noreply.github.com>
2024-04-30 19:07:10 +0800

    Update egg.cpp

--- ncurses/egg.cpp
+++ ncurses/egg.cpp
@@ -1,11 +1,13 @@
 #include "egg.h"
 
+//For playing the ASCII art viedo 
+
 void egg() {
-    const string relativePath = "store/storeframe_";
-    WINDOW* buffer = newpad(1000, 1000);
+    const string relativePath = "store/storeframe_"; //Locating the path of the size containing the ASCII art files
+    WINDOW* buffer = newpad(1000, 1000);   //Create a new buffer window of size 1000 x 1000 to avoid flickering
 
     for (int i = 0; i <= 606; i++) {
-        ifstream file(relativePath + to_string(i) + ".txt");
+        ifstream file(relativePath + to_string(i) + ".txt");          //606 is the total number of the frames of the ASCII art
 
         if (file) {
             string line;
